b7d35140c04d9b2d24f80f0a7f4fdac2d0a01923
nanja <nanjarandriamalala98@gmail.com>
2025-01-09 14:51:40 +0100

    PAge2

--- mon-ucref/src/App.js
+++ mon-ucref/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Welcome from './components/Pages/Welcom/Welcome.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Pages/Home/Home.js';
+
 
 function App() {
   return (
-    <Welcome />
+    <div>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+    </div>
+  
   );
 }
 
